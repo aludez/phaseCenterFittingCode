bea02f236f4346476e9317d8bbf6cbea23049272
Andrew Ludwig <drewbaludwig@gmail.com>
2017-02-22 11:10:36 -0600

    fixed average wais maker

--- macros/makeWaisAveragePulses.C
+++ macros/makeWaisAveragePulses.C
@@ -9,6 +9,7 @@
 #include "TSystem.h"
 #include "TTree.h"
 #include "TChain.h"
+#include "TString.h"
 #include "RawAnitaHeader.h"
 #include "CorrelationSummaryAnita3.h"
 #include "TTreeIndex.h"
@@ -28,25 +29,11 @@ void makeWaisAveragePulses(int centAnt)
 	TGraph* mid = 0;
 	TGraph* bot = 0;
 	makeAverageGraphs(top, mid, bot, centAnt);
-	
-	top->SetName(Form("top%d", centAnt));
-	mid->SetName(Form("mid%d", centAnt));
-	bot->SetName(Form("bot%d", centAnt));
-	
-	TFile f(Form("averageGraphs/waisAverage%d.root", centAnt), "RECREATE");
-	top->Write();
-	mid->Write();
-	bot->Write();
-	f.Close();
-
 }
 
 void makeAverageGraphs(TGraph* top, TGraph* mid, TGraph* bot, int center)
 {
 	int i = 0;
-	TGraph* gtemptop = 0;
-	TGraph* gtempmid = 0;
-	TGraph* gtempbot = 0;
 	TGraph* tops[2] = {NULL};	
 	TGraph* mids[2] = {NULL};	
 	TGraph* bots[2] = {NULL};	
@@ -54,16 +41,15 @@ void makeAverageGraphs(TGraph* top, TGraph* mid, TGraph* bot, int center)
 	TChain* headChain = new TChain("headTree");
 	TChain* evChain = new TChain("eventTree");
 	WaveCalType::WaveCalType_t cal = WaveCalType::kDefault;
-	TString datadir = "/project/kicp/avieregg/anitaIV/flight1617/root/";
+	TString datadir = "/home/abl/anita/";
 	char corrName[FILENAME_MAX];
 	RawAnitaEvent* event = 0;
 	RawAnitaHeader* header = 0;
 	TTreeIndex* ind = 0;
 	CorrelationSummaryAnita3 * corr = 0;
 	int pol = 0;
-	UsefulAnitaEvent* uae = 0;
 
-	for (int run = 120; run < 125; run++)
+	for (int run = 125; run < 126; run++)
 	{
 		sprintf(corrName, "corrTrees/run%dCorrTree.root",run);
 		corrChain->Add(corrName);
@@ -90,18 +76,15 @@ void makeAverageGraphs(TGraph* top, TGraph* mid, TGraph* bot, int center)
 		int entryIndex = ind->GetEntryNumberWithIndex(corr->eventNumber,0);
 		headChain->GetEntry(entryIndex);
 		evChain->GetEntry(entryIndex);
-		printf("stuck here %d\n", header->eventNumber);	
-		uae = new UsefulAnitaEvent(event, cal, header);	
-		printf("or here %d\n", j);	
-		gtemptop = uae->getGraph(AnitaRing::kTopRing, center, AnitaPol::kHorizontal);
-		gtempmid = uae->getGraph(AnitaRing::kMiddleRing, center, AnitaPol::kHorizontal);
-		gtempbot = uae->getGraph(AnitaRing::kBottomRing, center, AnitaPol::kHorizontal);
+		UsefulAnitaEvent uae(event, cal, header);	
+		TGraph* gtemptop = uae.getGraph(AnitaRing::kTopRing, center, AnitaPol::kHorizontal);
+		TGraph* gtempmid = uae.getGraph(AnitaRing::kMiddleRing, center, AnitaPol::kHorizontal);
+		TGraph* gtempbot = uae.getGraph(AnitaRing::kBottomRing, center, AnitaPol::kHorizontal);
 		if(i == 0)
 		{
 			tops[0] = FFTtools::getInterpolatedGraph(gtemptop, 1/(2.6*16));
 			mids[0] = FFTtools::getInterpolatedGraph(gtempmid, 1/(2.6*16));
 			bots[0] = FFTtools::getInterpolatedGraph(gtempbot, 1/(2.6*16));
-			delete uae;
 			i++;
 			continue;
 		}
@@ -111,37 +94,41 @@ void makeAverageGraphs(TGraph* top, TGraph* mid, TGraph* bot, int center)
 		gtemptop = FFTtools::correlateAndAverage(2, tops);
 		gtempmid = FFTtools::correlateAndAverage(2, mids);
 		gtempbot = FFTtools::correlateAndAverage(2, bots);
+	
+		delete tops[0]; delete mids[0]; delete bots[0]; 
 		
-		tops[0] = (TGraph*) gtemptop->Clone();
-		mids[0] = (TGraph*) gtempmid->Clone();
-		bots[0] = (TGraph*) gtempbot->Clone();
-		for (int k = 0 ; k < tops[0]->GetN(); k++)
-		{
-			tops[0]->GetY()[k] *= 2;
-			mids[0]->GetY()[k] *= 2;
-			bots[0]->GetY()[k] *= 2;
-		}
-		delete uae;
+		tops[0] = new TGraph(gtemptop->GetN(), gtemptop->GetX(), gtemptop->GetY());
+		mids[0] = new TGraph(gtempmid->GetN(), gtempmid->GetX(), gtempmid->GetY());
+		bots[0] = new TGraph(gtempbot->GetN(), gtempbot->GetX(), gtempbot->GetY());
+		
+		for (int k = 0 ; k < tops[0]->GetN(); k++) tops[0]->GetY()[k] *= 2;
+		for (int k = 0 ; k < mids[0]->GetN(); k++) mids[0]->GetY()[k] *= 2;
+		for (int k = 0 ; k < bots[0]->GetN(); k++) bots[0]->GetY()[k] *= 2;
 		i++;
+		delete gtemptop; delete gtempmid; delete gtempbot;
 	}
 	
-	top = (TGraph*) tops[0]->Clone();
-	mid = (TGraph*) mids[0]->Clone();
-	bot = (TGraph*) bots[0]->Clone();
+	top = new TGraph(tops[0]->GetN(), tops[0]->GetX(), tops[0]->GetY());
+	mid = new TGraph(mids[0]->GetN(), mids[0]->GetX(), mids[0]->GetY());
+	bot = new TGraph(bots[0]->GetN(), bots[0]->GetX(), bots[0]->GetY());
 
-	for (int k = 0 ; k < top->GetN(); k++)
-	{
-		top->GetY()[k] /= i;
-		mid->GetY()[k] /= i;
-		bot->GetY()[k] /= i;
-	}
-	delete[] tops[0];
-	delete[] tops[1];
-	delete[] mids[0];
-	delete[] mids[1];
-	delete[] bots[0];
-	delete[] bots[1];
-	delete gtemptop;
-	delete gtempmid;
-	delete gtempbot;
+
+	for (int k = 0 ; k < top->GetN(); k++) top->GetY()[k] /= double(i);
+	for (int k = 0 ; k < mid->GetN(); k++) mid->GetY()[k] /= double(i);
+	for (int k = 0 ; k < bot->GetN(); k++) bot->GetY()[k] /= double(i);
+	
+	top->SetName(Form("top%d", center));
+	mid->SetName(Form("mid%d", center));
+	bot->SetName(Form("bot%d", center));
+	
+	TFile f(Form("averageGraphs/waisAverage%d.root", center), "RECREATE");
+	top->Write();
+	mid->Write();
+	bot->Write();
+	f.Close();
+	
+	delete tops[0]; delete tops[1];
+	delete mids[0]; delete mids[1];
+	delete bots[0]; delete bots[1];
+	delete top; delete mid; delete bot;
 }
